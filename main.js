7be6772bec4b53cf595c3c30c8820447c16e71e9
Niko Kreissl <nikokreissl@gmail.com>
2023-02-09 09:57:50 +0100

    Cleared main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 // Query
 
 // Open answer script
-
-const questionCard = document.querySelector('[data-js="question-card"]');
-
-const answerButton = document.querySelector('[data-js="answer-button"]');
-
-answerButton.addEventListener("click", () => {
-  questionCard.classList.toggle("question-card__answer--shown");
-});
