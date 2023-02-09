b1a11c16afc3a3cb6bb032aaded95fab321cb418
Niko Kreissl <nikokreissl@gmail.com>
2023-02-09 09:57:10 +0100

    Open dark-mode.css, cleared dark-mode JS

--- main.js
+++ main.js
@@ -9,33 +9,3 @@ const answerButton = document.querySelector('[data-js="answer-button"]');
 answerButton.addEventListener("click", () => {
   questionCard.classList.toggle("question-card__answer--shown");
 });
-
-// Dark mode script
-
-// Elements
-const bodyElement = document.querySelector('[data-js="body"]');
-const headerElement = document.querySelector('[data-js="header"]');
-/* Question Card already defined */
-const questionAnswerElement = document.querySelector(
-  '[data-js="question-answer"]'
-);
-const questionCardBookmark = document.querySelector(
-  '[data-js="bookmark-icon"]'
-);
-const navigationBlock = document.querySelector('[data-js="navigation"]');
-const navigationItems = document.querySelector('[data-js="navigation-items"]');
-
-// Button
-const themeButton = document.querySelector('[data-js="theme-button"]');
-
-// Event Listener
-
-themeButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("body--dark-mode");
-  headerElement.classList.toggle("header--dark-mode");
-  questionCard.classList.toggle("question-card--dark-mode");
-  questionAnswerElement.classList.toggle("question-card__answer--dark-mode");
-  questionCardBookmark.classList.toggle("question-card__bookmark--dark-mode");
-  navigationBlock.classList.toggle("nav--dark-mode");
-  navigationItems.classList.toggle("nav-items--dark-mode");
-});
